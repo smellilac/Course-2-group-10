913f4e0f6dc2ae2cb0864441220b1152d7bbb898
Lytrum <116316417+LitrumCoder@users.noreply.github.com>
2024-10-22 21:18:04 +0300

    Add files via upload

---
+++ Course/2nd year/Lesson9/solution practic/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace base1
+{
+    class main
+    {
+        public static void Main(string[] args)
+        { 
+            Person pers1 = new Person(10,"Tom");
+            Person pers2 = new Person(99,"Eger");
+            Person pers3 = new Person(-45,"Znif");
+            Person pers4 = new Person(21,"David");
+            Person[] pers = {pers1,pers2,pers3,pers4};
+            foreach (Person person in pers)
+            { 
+                person.Introduce();
+            }
+        }
+
+        
+    }  
+}
+class Person
+{
+    public Person(int age, string name)
+    {
+        Age = age;
+        Name = name;
+    
+    }
+    public int Age
+    {
+        set
+        {
+            if (value < 1 || value > 120)
+                Console.WriteLine("Возраст должен быть в диапазоне от 1 до 120");
+            else
+                age = value;
+        }
+        get { return age; }
+    }
+    public int age = 1;
+    public string? Name;
+    public void Introduce() => Console.WriteLine($"Привет, мое имя{Name}");
+
+}
+class Employee : Person
+{
+    public Employee(int age, string name, string position) : base(age, name)
+    {
+        Position = position;
+    }
+    public string Position { get; set; } 
+}
